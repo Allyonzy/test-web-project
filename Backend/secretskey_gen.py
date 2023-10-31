fdbd3072962f3311401841b14c3e17d0eaa52a7c
Allyonzy <64588181+Allyonzy@users.noreply.github.com>
2023-10-31 20:38:37 +0300

    Комментарии к библиотекам

--- Backend/secretskey_gen.py
+++ Backend/secretskey_gen.py
@@ -1,4 +1,5 @@
-import secrets
-import string
+import secrets # зашифрованный доступ к авторизации сайта
+import string # работа со строками и кодивка строки
+
 print("".join(secrets.choice(string.digits + string.ascii_letters +
       string.punctuation) for i in range(100)))
